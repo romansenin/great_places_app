939a8296292c5c5d4d870e8f39cbbbd6ac5d9bf5
Roman Senin <roman@launchbadge.com>
2021-06-09 12:26:22 -0700

    add location input and fetch user coordinates

--- lib/screens/add_place_screen.dart
+++ lib/screens/add_place_screen.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:great_places_app/providers/great_places.dart';
+import 'package:great_places_app/widgets/location_input.dart';
 import 'package:provider/provider.dart';
 import 'package:great_places_app/widgets/image_input.dart';
 
@@ -52,6 +53,8 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
                       height: 10,
                     ),
                     ImageInput(_selectImage),
+                    SizedBox(height: 10),
+                    LocationInput(),
                   ],
                 ),
               ),
